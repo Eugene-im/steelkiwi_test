303a68e683d8d523373ac0f8896969ec740d2e3b
Eugene <em.92@mail.ru>
2017-05-25 23:36:50 +0300

    skeleton with piece of meat

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-$( function() {$( "#tabs" ).tabs()})
+$('.block_slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+
+$('.about_our_equip__item_slide').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
