77735e4f3a4f55465e8ff07df48080962884112b
Eugene <em.92@mail.ru>
2017-05-29 04:02:11 +0300

    zoom and media querrry left

--- js/script.js
+++ js/script.js
@@ -15,5 +15,26 @@ $(document).ready(function(){
     navText: ['<div class="slide-prev"></div>','<div class="slide-next"></div>']
   });
   $('img.myImage1').zoomify({ scale: 1 });
+  
+  $('#a1').click(function(){
+  	$('#t1').toggleClass('display_on');
+  	$('#'+$(this).attr('id')+'.title_arrow').toggleClass('rotate');
+})
+  $('#a2').click(function(){
+  	$('#t2').toggleClass('display_on');
+  	$('#'+$(this).attr('id')+'.title_arrow').toggleClass('rotate');
+})
+  $('#a3').click(function(){
+  	$('#t3').toggleClass('display_on');
+  	$('#'+$(this).attr('id')+'.title_arrow').toggleClass('rotate');
+})
+  $('#a4').click(function(){
+  	$('#t4').toggleClass('display_on');
+  	$('#'+$(this).attr('id')+'.title_arrow').toggleClass('rotate');
+})
+  $('.menu_320').click(function(){
+  	$('.menu_320_list').toggle();
+  	// $('#'+$(this).attr('id')+'.title_arrow').toggleClass('rotate');
+})
 });
 
